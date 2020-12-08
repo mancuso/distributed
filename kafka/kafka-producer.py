1a30b2fb7a1547c81bae6108f18e8ea1704e9dcb
mancuso <intercepted@gmail.com>
2020-12-08 18:57:01 -0500

    refactor

--- kafka/kafka-producer.py
+++ kafka/kafka-producer.py
@@ -2,11 +2,13 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
+TOPIC='microservice'
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 
 for e in range(1000):
     data = {'number' : e}
-    producer.send('microservice', value=data)
+    producer.send(TOPIC, value=data)
     sleep(5)
