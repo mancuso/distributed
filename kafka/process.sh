f72f40ee94bc29846f0b9f9fa2d814b76c28ba4e
mancuso <intercepted@gmail.com>
2020-12-09 20:53:02 -0500

    add producer

--- kafka/process.sh
+++ kafka/process.sh
@@ -4,3 +4,5 @@ tar zxvf kafka_2.12-2.6.0.tgz
 mv kafka_2.12-2.6.0 /usr/local/kafka
 vim /etc/systemd/system/zookeeper.service
 vim /etc/systemd/system/kafka.service
+/usr/local/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic microservice
+/usr/local/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic microservice
