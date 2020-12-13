aecf8275b63e5cabe5e508fabb8d4f2f0d37621e
mancuso <intercepted@gmail.com>
2020-12-13 17:34:22 -0500

    refactor consul

--- consul/deploy-service.sh
+++ consul/deploy-service.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "Pull Consul  Docker Image"
-docker pull consul
-echo "Starting Consul Server"
+echo "Creating Consul Server"
 docker run \
     -d \
     -p 8500:8500 \
@@ -9,22 +7,21 @@ docker run \
     --name=sun \
     consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
 
-echo "Starting Consul  Client"
+echo "Creating Consul Client"
 docker run \
-    -d
+    -d  \
     --name=earth \
     consul agent -node=client-1 -join=172.17.0.2
 
-echo "Pull Service"
-docker pull hashicorp/counting-service:0.0.2
-echo "Registering Service"
-
+echo "Creating Counting Service"
 docker run \
    -p 9001:9001 \
    -d \
    --name=moon \
    hashicorp/counting-service:0.0.2
+
+echo "Updating Consul Client Configuration with New Service"
 docker exec earth /bin/sh -c "echo '{\"service\": {\"name\": \"counting\", \"tags\": [\"go\"], \"port\": 9001}}' >> /consul/config/counting.json"
 
-echo "Reload Consol Config"
+echo "Reloading Consult Client Configuration"
 docker exec earth consul reload
